8bb9850cc374caa612621ce6a8ccab780fcb56da
ZenArtZ98 <sergey.volkov98@yandex.ru>
2024-05-06 23:23:08 +0500

    red date ready

--- app/src/main/java/com/zenartz/todo/TaskAdapter.kt
+++ app/src/main/java/com/zenartz/todo/TaskAdapter.kt
@@ -1,10 +1,13 @@
 package com.zenartz.todo
 
 import android.content.Context
+import android.graphics.Color
 import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import java.util.Locale
@@ -26,6 +29,7 @@ class TaskAdapter(private val context: Context, private var _tasks: MutableList<
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         val task = _tasks[position]
+        holder.bind(task)
         holder.taskTitle.text = task.name
         holder.taskDescription.text = task.description
         val dateFormat = SimpleDateFormat("dd.MM.yy", Locale.getDefault())
@@ -51,5 +55,23 @@ class TaskAdapter(private val context: Context, private var _tasks: MutableList<
         val taskDescription: TextView = itemView.findViewById(R.id.task_description)
         val taskDueDate: TextView = itemView.findViewById(R.id.task_due_date)
         // Declare other views here
+        private val titleTextView: TextView = itemView.findViewById(R.id.task_name)
+        private val descriptionTextView: TextView = itemView.findViewById(R.id.task_description)
+        private val dateTextView: TextView = itemView.findViewById(R.id.task_due_date)
+//        private val completedCheckBox: CheckBox = itemView.findViewById(R.id.completed_check_box)
+
+        fun bind(task: Task) {
+            titleTextView.text = task.name
+            descriptionTextView.text = task.description
+//            completedCheckBox.isChecked = task.isCompleted
+
+            val currentDate = Calendar.getInstance().time
+            if (task.dueDate.before(currentDate)) {
+                dateTextView.setTextColor(Color.RED)
+            } else {
+                dateTextView.setTextColor(itemView.context.getColor(R.color.black))
+            }
+            dateTextView.text = task.dueDate.toString()
+        }
     }
 }
